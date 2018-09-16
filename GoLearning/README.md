29baa4d55607d528c4bfc5720ca9a289d5806dc2
Rushing <liming20110711@163.com>
2018-09-17 07:38:20 +0800

    update:20180917-01

---
+++ GoLearning/README.md
@@ -0,0 +1,46 @@
+# example01
+
+## 定义变量
+
+- 使用var默认值
+
+```go
+var a,b,c, bool
+```
+
+- 使用var指定初始值
+
+```go
+var s1,s2 string = "hello", "world"
+```
+
+- 可放在函数内，或者直接放包内
+- 使用var集中定义变量
+- 让编译器自动决定类型
+
+```go
+var a,b,i,s1,s2 = true,false,3,"hello","world"
+```
+
+- 使用`:=`定义变量
+
+```go
+a,b,i,s1,s2 := true,false,3,"hello","world"
+```
+
+- `:=`方式只能在函数内使用
+
+# example02
+
+## 內建变量类型
+
+- bool,string
+- (u)int,(u)int8,(u)int16,(u)int32,(u)int64,(u)intptr
+- byte,rune
+- float32,float64,complex64,complex128
+
+## 复数
+
+- i =  $\sqrt{-1}$
+- 复数： 3 + 4i
+
---
+++ GoLearning/example/example01/basic.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+var aa = 3
+var ss = "kkk"
+
+var (
+	aaa = 3
+	sss = "kkk"
+	bbb  = true
+)
+
+// 在func之外不能如下定义变量
+// bb := true
+
+func variableZeroValue() {
+	var a int
+	var s string
+	fmt.Printf("%d %q\n", a, s)
+}
+
+func variableInitialValue() {
+	var a, b int = 3, 4
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
+func variableTypeDeduction() {
+	var a, b, c, s = 3, 4, true, "def"
+	fmt.Println(a, b, c, s)
+}
+
+func variableShorter() {
+	a, b, c, s := 3, 4, true, "def"
+	b = 5
+	fmt.Println(a, b, c, s)
+}
+
+func main() {
+	fmt.Println("Hello world")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
+	fmt.Println(aa, ss, aaa, sss, bbb)
+}
